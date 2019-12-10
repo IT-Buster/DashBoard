b0fe9ed991f6265c09999f03a6c6b770011ae47f
Kamzelski Bartosz <b_kamzelski@B-KRK.local>
2019-12-10 15:33:42 +0100

    React Grid Chart installed

--- src/Home.js
+++ src/Home.js
@@ -20,7 +20,7 @@ export const Home = () => (
         <Card.Body>
           <Card.Title>Secondary Card Title</Card.Title>
           <Card.Text>
-          <path style="" fill="#7f7f7f" stroke="none" d="M53.125,120L25,120A75,75,0,0,1,175,120L146.875,120A46.875,46.875,0,0,0,53.125,120Z" fill-opacity="0.5" filter="url(#inner-shadow-gauge_64)"></path>
+          
         </Card.Text>
         </Card.Body>
         <Card.Footer>
--- src/NoMatch.js
+++ src/NoMatch.js
@@ -1,18 +1,36 @@
 import React  from 'react'
+import { Chart, ArgumentAxis, ValueAxis, LineSeries } from "@devexpress/dx-react-chart-material-ui";
 
 export const NoMatch = () => (
-        <div>
-      
-        <footer class="app-footer">
-        <div>
-          <a href="https://coreui.io">CoreUI</a>
-          <span>&copy; 2018 creativeLabs.</span>
-        </div>
-        <div class="ml-auto">
-          <span>Powered by</span>
-          <a href="https://coreui.io">CoreUI</a>
-        </div>
-      </footer>
-
-        </div>
+  <Chart
+      data={[
+        { argument: 1, value: 10 },
+        { argument: 2, value: 20 },
+        { argument: 3, value: 30 }
+      ]}
+    >
+      <ArgumentAxis />
+      <ValueAxis />
+      <LineSeries valueField="value" argumentField="argument" />
+    </Chart>
 )
+
+/*
+import { Chart, ArgumentAxis, ValueAxis, LineSeries } from "@devexpress/dx-react-chart-material-ui";
+// import { Chart, ArgumentAxis, ValueAxis, LineSeries } from "@devexpress/dx-react-chart-bootstrap4";
+
+const App = () => (
+  <Chart
+      data={[
+        { argument: 1, value: 10 },
+        { argument: 2, value: 20 },
+        { argument: 3, value: 30 }
+      ]}
+    >
+      <ArgumentAxis />
+      <ValueAxis />
+      <LineSeries valueField="value" argumentField="argument" />
+    </Chart>
+);
+
+*/
